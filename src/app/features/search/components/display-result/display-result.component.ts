933a55c73183a6d0758cb62e48586fe624e62d8b
adpusel <adrien.pusel@gmail.com>
2020-01-13 18:21:51 +0100

    - add stuffc

---
+++ src/app/features/search/components/display-result/display-result.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-result',
+  template: `
+    <p>
+      display-result works!
+    </p>
+  `,
+  styles: []
+})
+export class DisplayResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
