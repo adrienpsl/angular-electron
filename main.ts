7a45ede7e9dd5b4a7d0462ea71c98193de8821a7
adrien <adrien.pusel@gmail.com>
2020-01-15 15:24:58 +0100

    add config to use deeplink

--- main.ts
+++ main.ts
@@ -1,11 +1,44 @@
 import { app, BrowserWindow, screen } from 'electron';
-import * as path from 'path';
-import * as url from 'url';
-import session = Electron.session;
+import * as path                      from 'path';
+import * as url                       from 'url';
 
 let win: BrowserWindow = null;
-const args = process.argv.slice(1),
-    serve = args.some(val => val === '--serve');
+const args  = process.argv.slice( 1 ),
+      serve = args.some( val => val === '--serve' );
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Deep linked url
+let deeplinkingUrl;
+
+// Force Single Instance Application
+const gotTheLock = app.requestSingleInstanceLock();
+if ( gotTheLock ) {
+  app.on( 'second-instance', ( e, argv ) => {
+    // Someone tried to run a second instance, we should focus our window.
+
+    // Protocol handler for win32
+    // argv: An array of the second instance’s (command line / deep linked)
+    // arguments
+    if ( process.platform == 'win32' ) {
+      // Keep only command line / deep linked arguments
+      deeplinkingUrl = argv.slice( 1 );
+    }
+    logEverywhere( 'app.makeSingleInstance# ' + deeplinkingUrl );
+
+    if ( mainWindow ) {
+      if ( mainWindow.isMinimized() ) {
+        mainWindow.restore();
+      }
+      mainWindow.focus();
+    }
+  } );
+} else {
+  app.quit();
+  return;
+}
 
 function createWindow(): BrowserWindow {
 
@@ -13,49 +46,49 @@ function createWindow(): BrowserWindow {
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
   // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
-    webPreferences: {
-      nodeIntegration: true,
-      allowRunningInsecureContent: (serve) ? true : false,
-    },
-  });
+  win = new BrowserWindow( {
+    x : 0,
+    y : 0,
+    width : size.width,
+    height : size.height,
+    webPreferences : {
+      nodeIntegration : true,
+      allowRunningInsecureContent : ( serve ) ? true : false
+    }
+  } );
 
-  if (serve) {
-    require('electron-reload')(__dirname, {
-      electron: require(`${__dirname}/node_modules/electron`)
-    });
-    win.loadURL('http://localhost:4200');
+  if ( serve ) {
+    require( 'electron-reload' )( __dirname, {
+      electron : require( `${ __dirname }/node_modules/electron` )
+    } );
+    win.loadURL( 'http://localhost:4200' );
   } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    win.loadURL( url.format( {
+      pathname : path.join( __dirname, 'dist/index.html' ),
+      protocol : 'file:',
+      slashes : true
+    } ) );
   }
 
-  if (serve) {
+  if ( serve ) {
     // win.webContents.openDevTools();
   }
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on( 'closed', () => {
     // Dereference the window object, usually you would store window
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null;
-  });
+  } );
 
-  // session.defaultSession.webRequest.onBeforeRequest({urls}, (details, callback) => {
-  //   if (details.url.indexOf('7accc8730b0f99b5e7c0702ea89d1fa7c17bfe33') !== -1) {
-  //     callback({redirectURL: details.url.replace('7accc8730b0f99b5e7c0702ea89d1fa7c17bfe33', '57c9d07b416b5a2ea23d28247300e4af36329bdc')});
-  //   } else {
-  //     callback({cancel: false});
-  //   }
-  // });
+  // session.defaultSession.webRequest.onBeforeRequest({urls}, (details,
+  // callback) => { if
+  // (details.url.indexOf('7accc8730b0f99b5e7c0702ea89d1fa7c17bfe33') !== -1) {
+  // callback({redirectURL:
+  // details.url.replace('7accc8730b0f99b5e7c0702ea89d1fa7c17bfe33',
+  // '57c9d07b416b5a2ea23d28247300e4af36329bdc')}); } else { callback({cancel:
+  // false}); } });
 
   return win;
 }
@@ -65,26 +98,34 @@ try {
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+  app.on( 'ready', createWindow );
 
   // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
+  app.on( 'window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if ( process.platform !== 'darwin' ) {
       app.quit();
     }
-  });
+  } );
 
-  app.on('activate', () => {
+  app.on( 'activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
+    if ( win === null ) {
       createWindow();
     }
-  });
+  } );
 
-} catch (e) {
+} catch ( e ) {
   // Catch Error
   // throw e;
+}
+
+// Log both at dev console and at running node console instance
+function logEverywhere(s) {
+  console.log(s)
+  if (mainWindow && mainWindow.webContents) {
+    mainWindow.webContents.executeJavaScript(`console.log("${s}")`)
+  }
 }
